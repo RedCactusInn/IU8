68afec85a20ad21965e59cc4ffe4c24bfe7b6603
Timur Orbelian <Timur_Orbelyan@mentor.com>
2021-09-20 18:23:28 +0300

    w

--- TSiSA/lab_1/src/passive_search.py
+++ TSiSA/lab_1/src/passive_search.py
@@ -4,7 +4,7 @@ import time
 
 
 def function(x: float):
-    function_value = (1-x)*(1-x) + math.exp(x)
+    function_value = (1 - x) * (1 - x) + math.exp(x)
     return function_value
 
 
@@ -26,7 +26,7 @@ def find_minimum_optimal_passive_search(a_border: float, b_border: float, epsilo
     first_dot = (a_border, function_in_a)
     if function_in_b < function_in_a:
         current_x = b_border
-        delta = -epsilon
+        delta = -epsilon * 0.5
         first_dot = (b_border, function_in_b)
     x_min = current_x
     dots = [first_dot]
@@ -44,14 +44,33 @@ def find_minimum_optimal_passive_search(a_border: float, b_border: float, epsilo
         current_dot = (current_x, current_f)
         dots.append(current_dot)
 
-    print(f"INFO: minimum found in x = {x_min}")
+    return x_min, dots
+
+
+def plot_graph(f, a_border, b_border, step=0.1):
+    x = a_border
+    dots = []
+    while x_is_in_borders(x, a_border, b_border):
+        dots.append((x, f(x)))
+        x += step
+
+    plot_dots(dots)
+
+
+def plot_dots(dots):
     plt.scatter(*zip(*dots))
     plt.show()
 
 
 def main():
-    epsilon = 0.01
-    find_minimum_optimal_passive_search(-5.0, 2.0, epsilon)
+    delta_n = 0.05
+    epsilon = 2 * delta_n
+    a_border = -5.0
+    b_border = 2.0
+
+    x_min, dots = find_minimum_optimal_passive_search(a_border, b_border, epsilon)
+    plot_dots(dots)
+    plot_graph(function, a_border, b_border, step=0.1)
 
 
 if __name__ == "__main__":
